ab151e6816c12d035cce854cf8c34035bc9117b0
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-03-10 20:10:48 +0100

    feat: login added

--- src/pl/boniaszczuk/weekop/dao/UserDAOImpl.java
+++ src/pl/boniaszczuk/weekop/dao/UserDAOImpl.java
@@ -1,20 +1,29 @@
 package pl.boniaszczuk.weekop.dao;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+
+
 import pl.boniaszczuk.weekop.model.User;
 import pl.boniaszczuk.weekop.util.ConnectionProvider;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
 
     private static final String CREATE_USER =
-            "INSERT INTO user(username, email, password, is_active)" +
-                    " VALUES(:username, :email, :password, :active);";
+            "INSERT INTO user(username, email, password, is_active) VALUES(:username, :email, :password, :active);";
+    private static final String READ_USER =
+            "SELECT user_id, username, email, password, is_active FROM user WHERE user_id = :id";
+    private static final String READ_USER_BY_USERNAME =
+            "SELECT user_id, username, email, password, is_active FROM user WHERE username = :username";
     private NamedParameterJdbcTemplate template;
     public UserDAOImpl() {
         template = new NamedParameterJdbcTemplate(ConnectionProvider.getDataSource());
@@ -41,7 +50,10 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User read(Long primaryKey) {
-        return null;
+        User resultUser = null;
+        SqlParameterSource paramSource = new MapSqlParameterSource("id", primaryKey);
+        resultUser = template.queryForObject(READ_USER, paramSource, new UserRowMapper());
+        return resultUser;
     }
 
     @Override
@@ -61,6 +73,21 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        User resultUser = null;
+        SqlParameterSource paramSource = new MapSqlParameterSource("username", username);
+        resultUser = template.queryForObject(READ_USER_BY_USERNAME, paramSource, new UserRowMapper());
+        return resultUser;
+    }
+    private class UserRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(resultSet.getLong("user_id"));
+            user.setUsername(resultSet.getString("username"));
+            user.setEmail(resultSet.getString("email"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -24,7 +25,14 @@
       <ul class="nav navbar-nav navbar-right">
         <li class="active"><a href="#">Główna</a></li>
         <li><a href="#">Dodaj</a></li>
-        <li><a href="#">Zaloguj się</a></li>
+        <c:choose>
+          <c:when test="${not empty sessionScope.user}">
+            <li><a href="logout">Wyloguj się</a></li>
+          </c:when>
+          <c:otherwise>
+            <li><a href="login">Zaloguj się</a></li>
+          </c:otherwise>
+        </c:choose>
       </ul>
     </div>
 
